a85fcf2a5395f320dc1a2a6b74393bff019ec571
Carlos Ariel <ariels7801@gmail.com>
2018-01-26 13:25:37 -0600

    47. Pulir detalles de nuestro incrementador component

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -13,12 +13,27 @@ export class IncrementadorComponent implements OnInit {
   @Output('actualizaValor') cambioValor: EventEmitter<number> = new EventEmitter();
 
   constructor() {
-    console.log('Leyenda', this.leyenda);
-    console.log('Progreso', this.progreso);
   }
 
   ngOnInit() {
-    console.log('Leyenda', this.leyenda);
+  }
+
+  onChange(newValue: number) {
+
+    let elemHTML: any =  document.getElementsByName('progreso')[0];
+    console.log(elemHTML.value);
+
+    if (newValue >= 100) {
+      this.progreso = 100;
+    } else if (newValue <= 0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = newValue;
+    }
+
+    elemHTML.value = this.progreso;
+
+    this.cambioValor.emit(this.progreso);
   }
 
   cambiarValor(valor) {
