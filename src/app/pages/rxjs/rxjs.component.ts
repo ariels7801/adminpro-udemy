8d49876a733b43d6a056adb18e575fdf456b79ca
Carlos Ariel <ariels7801@gmail.com>
2018-01-28 21:43:08 -0600

    75. Método Retry de un observable

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -11,21 +11,26 @@ export class RxjsComponent implements OnInit {
   constructor() {
 
     let obs = new Observable(observer => {
+
       let contador = 0;
       let intervalo = setInterval(() => {
         contador += 1;
-        observer.next();
+        observer.next(contador);
+
+        if (contador === 2) {
+          clearInterval(intervalo);
+          observer.error('Auxilio');
+        }
 
         if (contador === 3) {
           clearInterval(intervalo);
           observer.complete();
-        } else {
-          observer.error('Auxilio');
         }
       }, 1000);
     });
 
-    obs.subscribe(
+    obs.retry(2)
+      .subscribe(
       numero => console.log('Subs ', numero),
       error => console.log('Error en el obs ', error),
       () => console.log('El observador termino !')
