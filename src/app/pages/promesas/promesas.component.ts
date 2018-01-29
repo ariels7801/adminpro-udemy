d46ea42f94a8a6f5b9f3e51dbe3f44697c2e08ba
Carlos Ariel <ariels7801@gmail.com>
2018-01-28 18:46:21 -0600

    72. Funciones que retornan promesas

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -9,25 +9,28 @@ import {reject} from 'q';
 export class PromesasComponent implements OnInit {
 
   constructor() {
-    let promesa = new Promise((resolve, reject) => {
+
+    this.contarTres()
+      .then(mensaje => console.log('Termino!', mensaje))
+      .catch(error => console.log('Error en la promesa ', error));
+  }
+
+  ngOnInit() {
+  }
+
+  contarTres(): Promise<boolean> {
+
+    return new Promise((resolve, reject) => {
       let contador = 0;
       let intervalo = setInterval(() => {
         contador += 1;
         console.log(contador);
         if (contador === 3) {
-          resolve();
+          resolve(true);
           clearInterval(intervalo);
         }
       }, 1000);
     });
-
-    promesa
-      .then(() => console.log('Termino!'))
-      .catch(error => console.log('Error en la promesa ', error ));
-
-  }
-
-  ngOnInit() {
   }
 
 }
