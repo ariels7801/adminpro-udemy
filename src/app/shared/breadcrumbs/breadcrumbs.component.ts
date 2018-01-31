a6bbc2a6a19c83d77419631e556acec294c1a87b
Carlos Ariel <ariels7801@gmail.com>
2018-01-31 12:55:38 -0600

    81. MetaTags, Título y Página actual

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router, ActivationEnd} from '@angular/router';
+import {Title, Meta, MetaDefinition} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,14 +9,27 @@ import {Router, ActivationEnd} from '@angular/router';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor(private router: Router) {
-    this.router.events
+  label: string = '';
+  constructor(private router: Router, private title: Title, private meta: Meta) {
+
+    this.getDataRoute()
+      .subscribe(data => {
+        this.label = data.titulo;
+        this.title.setTitle(this.label);
+
+        let metaTag: MetaDefinition = {
+          name: 'description',
+          content: this.label
+        };
+        this.meta.updateTag(metaTag);
+      });
+  }
+
+  getDataRoute() {
+    return this.router.events
       .filter(evento => evento instanceof ActivationEnd)
       .filter((evento: ActivationEnd) => evento.snapshot.firstChild === null)
-      .map((evento: ActivationEnd) => evento.snapshot.data)
-      .subscribe(event => {
-        console.log(event);
-      });
+      .map((evento: ActivationEnd) => evento.snapshot.data);
   }
 
   ngOnInit() {
