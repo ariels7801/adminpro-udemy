aff967590e95f48331b42ef23b67524cb9f4647d
Carlos Ariel <ariels7801@gmail.com>
2018-02-21 20:40:03 -0600

    148. Creando usuarios - FrontEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import {APP_ROUTES} from './app.routes';
 
 // Modulos //
 import {PagesModule} from './pages/pages.module';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 // Servicios //
 import {ServiceModule} from './services/service.module';
@@ -27,6 +27,7 @@ import { RegisterComponent } from './login/register.component';
     BrowserModule,
     PagesModule,
     FormsModule,
+    ReactiveFormsModule,
     ServiceModule,
     APP_ROUTES
   ],
