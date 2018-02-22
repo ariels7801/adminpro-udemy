4276d1ffa340317330ddaff8ff8284b882690dc6
Carlos Ariel <ariels7801@gmail.com>
2018-02-21 21:35:34 -0600

    149. Validando que las contraseñas sean iguales - ReactiveForms

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -15,6 +15,23 @@ export class RegisterComponent implements OnInit {
   constructor() {
   }
 
+  sonIguales(campo1: string, campo2: string) {
+    return (group: FormGroup) => {
+
+      let pass1 = group.controls[campo1].value;
+      let pass2 = group.controls[campo2].value;
+
+      if (pass1 === pass2) {
+        return null;
+      }
+
+      return {
+        sonIguales: true
+      };
+    };
+  }
+
+
   ngOnInit() {
     init_plugins();
 
@@ -24,11 +41,30 @@ export class RegisterComponent implements OnInit {
       password: new FormControl(null, Validators.required),
       password2: new FormControl(null, Validators.required),
       condiciones: new FormControl(false)
+    }, {validators: this.sonIguales('password', 'password2')});
+
+    this.forma.setValue({
+      nombre: 'Test',
+      correo: 'test@test.com',
+      password: '123',
+      password2: '123',
+      condiciones: true
     });
   }
 
   registrarUsuario() {
-    console.log(this.forma.value);
+
+    if (this.forma.invalid) {
+      return;
+    }
+
+    if (this.forma.value.condiciones) {
+      console.log('Debe aceptar las condiciones')
+      return;
+    }
+
+    console.log('Forma válida ' + this.forma.valid);
+    console.log(this.forma);
   }
 
 }
