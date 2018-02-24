a55b8ae2ee085950c7a3844a54aa0eaffd97c60e
Carlos Ariel <ariels7801@gmail.com>
2018-02-23 21:36:58 -0600

    156. Función de recuérdame

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,13 +13,20 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  recuerdame: boolean = false;
+  email: string;
+  recordar: boolean = false;
 
   constructor(private _usuarioService: UsuarioService, private router: Router) {
   }
 
   ngOnInit() {
     init_plugins();
+
+    this.email = localStorage.getItem('email') || '';
+
+    if (this.email.length >= 1) {
+      this.recordar = true;
+    }
   }
 
   ingresar(forma: NgForm) {
@@ -30,7 +37,7 @@ export class LoginComponent implements OnInit {
 
     let usuario = new Usuario(null, forma.value.email, forma.value.password);
 
-    this._usuarioService.login(usuario, forma.value.recuerdame)
+    this._usuarioService.login(usuario, forma.value.recordar)
       .subscribe(correcto => this.router.navigate(['/dashboard']));
   }
 
